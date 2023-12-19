6b00823459867fd36ebc2b84be570852d06684c3
carlosmoura0701 <154276642+carlosmoura0701@users.noreply.github.com>
2023-12-18 21:43:25 -0400

    Update streamlit_index.py

--- coding/employees/streamlit_index.py
+++ coding/employees/streamlit_index.py
@@ -1,5 +1,6 @@
 import pickle
 from pathlib import Path
+from streamlit_option_menu import option_menu
 
 import pandas as pd
 import streamlit as st
@@ -7,12 +8,12 @@ import streamlit_authenticator as stauth
 
 st.set_page_config(page_title="Hours Manager",layout="centered")
 
-# user authentication
+# autenticação de usuário
 
 names = ["Carlos Moura","Sheila Santana"]
 usernames = ["carlosmoura","sheilasantanta"]
 
-# passwords load
+# carregamento de senha
 
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open('rb') as file:
@@ -22,17 +23,27 @@ authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "login_c
 
 name, authentication_status, username = authenticator.login("Login","main")
 
+# condicionais login
 
 if authentication_status == False:
     st.error("Username/password is incorrect")
 if authentication_status == None:
     st.warning("Please enter your user name and password")    
+# login == true    
 if authentication_status:
 
     authenticator.logout("Logout","sidebar")
     st.sidebar.title(f"Welcome {name}")
 
-    add_selectbox = st.sidebar.selectbox("O que você gostaria de fazer?",("Criar Novo Funcionario", "Pesquisar Funcionario","Visualizar todos"),)
+# menu lateral
+    with st.sidebar:
+        selected = option_menu(
+            menu_title=None, #obrigatório ou None
+            options=["Resume","New Employee","Rain"]
+        )
+
+if selected == "Resume":
+    st.title(f"You have selected {selected}")
 
     with st.container():
         st.subheader("Funcionários cadastrados")
@@ -48,6 +59,11 @@ if authentication_status:
         df = pd.read_csv("coding/employees/csv/opReport1.csv",sep=";",decimal=",")
         df
 
+if selected == "New Employee":
+    st.title(f"You have selected {selected}")
+
+if selected == "Rain":
+    st.title(f"You have selected {selected}")
     with st.container():
         st.subheader("Chuva")
         st.write("mm de chuva / anos")
@@ -58,9 +74,4 @@ if authentication_status:
         st.write("---")
         dados = pd.read_csv("coding/employees/csv/chuva.csv",sep=";",decimal=",")
         st.area_chart(dados,x="mes",y="total")
-
-
-
-
-#st.write("Teste de [link](https://www.google.com/)")    
-      
+ 
