31811c976012fef4b3b385ece6396183a326b619
carlosmoura0701 <154276642+carlosmoura0701@users.noreply.github.com>
2024-01-24 22:19:26 -0400

    Update _generate_keys.py

--- employees/_generate_keys.py
+++ employees/_generate_keys.py
@@ -5,14 +5,19 @@ import streamlit_authenticator as stauth
 
 # nao esquecer de atualizar a lista de usuarios no _main.py
 
-names = ['Carlos Moura','Sheila Santana','Judas Escariodes']
-usernames = ['carlosmoura','sheilasantanta','judas']
-passwords = ['','','']
+
+usernames = ['Carlos Moura','Sheila Santana','Judas Escariodes']
+names = ['carlosmoura','sheilasantanta','judas']
+passwords = ['admin','admin','admin']
+
+credentials = {"usernames":{}}
+        
+for uname,name,pwd in zip(usernames,names,passwords):
+    user_dict = {"name": name, "password": pwd}
+    credentials["usernames"].update({uname: user_dict})
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open('wb') as file:
     pickle.dump(hashed_passwords,file)
-
-    
